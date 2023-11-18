a7a330beed0a151c84ce35ec593acd60d84364a6
cuong.nguyenduc <ndcqn02@gmail.com>
2023-11-18 23:56:39 +0700

    fix to deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['tailwindui.com', 'images.unsplash.com', 'lawnet.vn'],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lawnet.vn',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbgdpl.haiphong.gov.vn',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tuyengiao.vn',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdnimg.vietnamplus.vn',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
