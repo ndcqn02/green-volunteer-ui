948e963fad8109ba8ca1a8d6e1b184a04fa90021
ndcqn02 <ndcqn02@gmail.com>
2023-12-01 19:31:48 +0700

    merge ui final

--- src/app/pages/listPost/page.js
+++ src/app/pages/listPost/page.js
@@ -216,6 +216,7 @@ export default function Hero() {
                 href="#"
                 className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
               >
+                
                 2
               </a>
               <a
